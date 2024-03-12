3900b7ce2252b3cc39c1bc66c1bbac9bbfda8622
tinaylt <144871562+tinaylt@users.noreply.github.com>
2024-03-11 21:31:34 -0400

    updated html and javascript file
    this is to add audio function into javascript and try to add a reset function for the drag and drop function.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+(() => {
 let theDonut = document.querySelectorAll(".drag-zone img"),
     dropZone = document.querySelectorAll(".drop-zone"),
+    theAudio = document.querySelector("#audioA1"),
+    donutAudio = document.querySelectorAll("#donut-audio"),
+    close = document.querySelectorAll(".close"),
+    audioElements = [],
     draggedDonut;
 
 //Drag and Drop function://
@@ -7,11 +12,16 @@ let theDonut = document.querySelectorAll(".drag-zone img"),
     function handleStartDrag() {
         console.log('start drag this!', this);
         draggedDonut = this;
+
+        let newSrc = `assets/audio/${this.dataset.trackref}.mp3`;
+        theAudio.src = newSrc; 
+        theAudio.load();
     }
 
     function handleDragOver(event) {
         event.preventDefault();
         console.log('dragged over a zone!!!');
+
     }
 
     function handleDrop(event) {
@@ -20,6 +30,11 @@ let theDonut = document.querySelectorAll(".drag-zone img"),
             event.preventDefault();
             console.log('dropped on the Zone');
             this.appendChild(draggedDonut);
+            theAudio.currentTime = 0;
+            // theAudio.loop = true;
+            theAudio.classList.remove('.hidden');
+            theAudio.play();
+            
         }
 
         else {
@@ -27,7 +42,22 @@ let theDonut = document.querySelectorAll(".drag-zone img"),
         }
     }
 
+    function resetDonut() {
+        // If the donut was dropped into a drop zone, remove it and return it to its original position
+        if (draggedDonut.parentElement.classList.contains('drop-zone')) {
+            draggedDonut.parentElement.removeChild(draggedDonut);
+            document.querySelector('.drag-zone').appendChild(draggedDonut); 
+            theAudio.pause();
+            // Assuming there's only one .drag-zone element
+        }
+    }
+
+
 
 theDonut.forEach(donut => donut.addEventListener("dragstart", handleStartDrag));
 dropZone.forEach(donut => donut.addEventListener("dragover", handleDragOver));
 dropZone.forEach(donut => donut.addEventListener("drop", handleDrop));
+close.forEach(close => close.addEventListener("click", resetDonut));
+
+})();
+
